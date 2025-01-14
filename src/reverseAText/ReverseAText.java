18e374c22a7c038d68e8945b4d0db0dea7b09d07
gumburtu <aslanseval1986@gmail.com>
2025-01-14 10:48:15 +0000

    tersYazi

--- src/reverseAText/ReverseAText.java
+++ src/reverseAText/ReverseAText.java
@@ -1,5 +1,19 @@
-public class Main {
+package reverseAText;
+
+import java.util.Arrays;
+
+public class ReverseAText {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Yazilan metni ters cevirerek konsola yazan bir java kodu yaziniz...
+        String yazi = "Java cok guzel";
+        String tersYazi = "";
+
+        String[] harfler = yazi.split("");
+        System.out.println("Arrays.toString(harfler) = " + Arrays.toString(harfler));
+
+        for (int i = harfler.length - 1; i >= 0; i--) {
+            tersYazi += harfler[i]; //lezug koc avaJ  //lezug koc avaJ
+        }
+        System.out.println("Yazimizin tersten yazilisi = " + tersYazi);
     }
 }
